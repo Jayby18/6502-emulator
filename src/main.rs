96b92c975976d27b7f7df0030f01d560cbf29207
Jaden Accord <jbr.accord@gmail.com>
2023-08-16 22:13:56 +0200

    CPU outline

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ struct CPU {
     Y: u8,  // Y register
     SP: u8,   // stack pointer
     PC: u16,   // program counter
-    SR: u16,   // status register
+    SR: u8,   // status register
 }
 
 impl CPU {
-    fn new(A: u8, X: u8, Y: u8, SP: u8, PC: u16, SR: u16) -> Self {
+    fn new(A: u8, X: u8, Y: u8, SP: u8, PC: u16, SR: u8) -> Self {
         return CPU {
             A,
             X,
@@ -20,9 +20,69 @@ impl CPU {
         };
     }
 
+    fn connect_bus(n: &Bus) {
+        // 
+    }
+
+    // Write & read bus
+
+    fn write(bus: &mut Bus, addr: u16, data: u8) {
+        bus.write(addr, data);
+    }
+
+    fn read(bus: &Bus, addr: u16) -> u8 {
+        bus.read(addr, false)
+    }
+
+    // Flags
+
+    fn get_flag(f: Flags) -> u8 {
+
+    }
+
+    fn set_flag(f: Flags, v: bool) {
+
+    }
+
+    // Addressing modes
+
+    // Opcodes
+
+    // Clock
+    fn clock() {
+
+    }
+
+    // Reset
     fn reset(&mut self) {
         self.PC = 0xFFFC;
     }
+
+    // Interrupt request (irq)
+
+    // Not maskeable interrupt (nmi)
+
+    // Fetch data
+    fn fetch() -> u8 {
+        0
+    }
+
+    let mut fetched: u8 = 0x00;
+    let mut addr_abs: u16 = 0x000;
+    let mut addr_rel: u16 = 0x00;
+    let mut opcode: u8 = 0x00;
+    let mut cycles: u8 = 0;
+}
+
+enum Flags {
+    C = 0b0000_0001,    // carry
+    Z = 0b0000_0010,    // zero
+    I = 0b0000_0100,    // disable interrupt
+    D = 0b0000_1000,    // decimal
+    B = 0b0001_0000,    // break
+    U = 0b0010_0000,    // unused
+    V = 0b0100_0000,    // overflow
+    N = 0b1000_0000,    // negative
 }
 
 struct Bus {
@@ -59,7 +119,7 @@ impl Bus {
 
 fn main() {
     println!("Hello world");
-    let mut cpu: CPU = CPU::new(0x00, 0x00, 0x00, 0x00, 0x0000, 0x0000);
+    let mut cpu: CPU = CPU::new(0x00, 0x00, 0x00, 0x00, 0x0000, 0x00);
     cpu.reset();
     let mut bus: Bus = Bus::new(cpu);
 }
