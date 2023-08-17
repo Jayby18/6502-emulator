7240ce117914c935e7bd4a3c24a23ea482e5718c
Jaden Accord <jbr.accord@gmail.com>
2023-08-17 12:52:51 +0200

    Instruction struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // mod CPU;
 // mod Bus;
+// mod opcodes;
 
 struct CPU {
     // byte: u8, word: u16
@@ -9,11 +10,11 @@ struct CPU {
     sp: u8,   // stack pointer
     pc: u16,   // program counter
     sr: u8,   // status register
-    fetched: u8,
-    addr_abs: u16,
-    addr_rel: u16,
-    opcode: u8,
-    cycles: u8,
+    fetched: u8,    // data that has been fetched by fetch()
+    addr_abs: u16,  // memory address to read from (absolute)
+    addr_rel: u16,  // memory address to read from (relative)
+    opcode: u8,     // current opcode
+    cycles: u8,     // cycles left to run
     bus: Option<Bus>,
 }
 
@@ -76,15 +77,105 @@ impl CPU {
     }
 
     // Addressing modes
+    fn IMP() -> u8 {}
+    fn ZP0() -> u8 {}
+    fn ZPY() -> u8 {}
+    fn ABS() -> u8 {}
+    fn ABY() -> u8 {}
+    fn IZX() -> u8 {}
+    fn IMM() -> u8 {}
+    fn ZPX() -> u8 {}
+    fn REL() -> u8 {}
+    fn ABX() -> u8 {}
+    fn IND() -> u8 {}
+    fn IZY() -> u8 {}
 
     // Opcodes
+    fn ADC() -> u8 {}
+    fn AND() -> u8 {}
+    fn ASL() -> u8 {}
+    fn BBR() -> u8 {}
+    fn BBS() -> u8 {}
+    fn BCC() -> u8 {}
+    fn BCS() -> u8 {}
+    fn BEQ() -> u8 {}
+    fn BIT() -> u8 {}
+    fn BMI() -> u8 {}
+    fn BNE() -> u8 {}
+    fn BPL() -> u8 {}
+    fn BRA() -> u8 {}
+    fn BVC() -> u8 {}
+    fn BVS() -> u8 {}
+    fn CLC() -> u8 {}
+    fn CLD() -> u8 {}
+    fn CLI() -> u8 {}
+    fn CLV() -> u8 {}
+    fn CMP() -> u8 {}
+    fn CPX() -> u8 {}
+    fn CPY() -> u8 {}
+    fn DEC() -> u8 {}
+    fn DEX() -> u8 {}
+    fn EOR() -> u8 {}
+    fn INC() -> u8 {}
+    fn INX() -> u8 {}
+    fn INY() -> u8 {}
+    fn JMP() -> u8 {}
+    fn JSR() -> u8 {}
+    fn LDA() -> u8 {}
+    fn LDX() -> u8 {}
+    fn LDY() -> u8 {}
+    fn LSR() -> u8 {}
+    fn NOP() -> u8 {}
+    fn ORA() -> u8 {}
+    fn PHA() -> u8 {}
+    fn PHP() -> u8 {}
+    fn PHX() -> u8 {}
+    fn PHY() -> u8 {}
+    fn PLA() -> u8 {}
+    fn PLP() -> u8 {}
+    fn PLX() -> u8 {}
+    fn PLY() -> u8 {}
+    fn RMB() -> u8 {}
+    fn ROL() -> u8 {}
+    fn ROR() -> u8 {}
+    fn RTI() -> u8 {}
+    fn RTS() -> u8 {}
+    fn SBC() -> u8 {}
+    fn SEC() -> u8 {}
+    fn SED() -> u8 {}
+    fn SEI() -> u8 {}
+    fn SMB() -> u8 {}
+    fn STA() -> u8 {}
+    fn STP() -> u8 {}
+    fn STX() -> u8 {}
+    fn STY() -> u8 {}
+    fn STZ() -> u8 {}
+    fn TAX() -> u8 {}
+    fn TAY() -> u8 {}
+    fn TRB() -> u8 {}
+    fn TSB() -> u8 {}
+    fn TXA() -> u8 {}
+    fn TXS() -> u8 {}
+    fn TYA() -> u8 {}
+    fn WAI() -> u8 {}
+    fn XXX() -> u8 {}
 
     // Clock
     fn clock(&mut self) {
         if self.cycles == 0 {
             self.opcode = self.read(&self.bus.as_ref().unwrap(), self.pc);
             self.pc += 1;
+
+            // TODO: Get Starting number of cycles
+            // self.cycles = lookup[self.opcode].cycles;
+
+            // let additional_cycle1: u8 = (this->*lookup[opcode].addrmode)();
+            // let additional_cycle2: u8 = (this->*lookup[opcode].operate)();
+
+            // self.cycles += (additional_cycle1 & additional_cycle2);
         }
+
+        self.cycles -= 1;
     }
 
     // Reset
@@ -93,17 +184,17 @@ impl CPU {
     }
 
     // Interrupt request (irq)
-    fn irq() {
+    fn irq(&mut self) {
 
     }
 
     // Not maskeable interrupt (nmi)
-    fn nmi() {
+    fn nmi(&mut self) {
 
     }
 
     // Fetch data
-    fn fetch() -> u8 {
+    fn fetch(&mut self) -> u8 {
         0
     }
 }
@@ -145,6 +236,24 @@ impl Bus {
             return self.ram[addr as usize];
         } else {
             panic!("Memory access out of bounds. RAM can only access between 0x0000 and 0xFFFF.");
+        }
+    }
+}
+
+struct Instruction {
+    name: String,
+    operate: Option<&fn()>,
+    addr_mode: Option<&fn()>,   // 
+    cycles: u8,
+}
+
+impl Instruction {
+    fn new(name: &str) -> Self {
+        Instruction {
+            name: String::from(name),
+            operate: None,
+            addr_mode: None,
+            cycles: 0,
         }
     }
 }
